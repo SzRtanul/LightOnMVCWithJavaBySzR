bd6b275338dd0bfef947034f6f74f92d76f999a2
Szabó Roland <szrtanul@gmail.com>
2025-11-12 14:07:53 +0100

    MVC sablon

---
+++ src/controller/LightOnController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.LightOn;
+import view.LightOnGUIView;
+
+/**
+ *
+ * @author SzabóRoland(SZF_2024
+ */
+public class LightOnController {
+    public LightOnController(LightOn model, LightOnGUIView view){
+    
+    }
+}
---
+++ src/model/LightOn.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author SzabóRoland(SZF_2024
+ */
+public class LightOn {
+    public LightOn(){
+    
+    }
+}
